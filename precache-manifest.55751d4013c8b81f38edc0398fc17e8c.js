67cb8642a5685b03968b78ff10072de9c573554e
yafanisonya <yafanison@gmail.com>
2020-06-25 21:12:22 +0800

    deploy

---
+++ precache-manifest.55751d4013c8b81f38edc0398fc17e8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1fc32127568340552d",
+    "url": "/ledger-website/css/app.45726e07.css"
+  },
+  {
+    "revision": "a45d9b103bb823eef71c4649924d6b47",
+    "url": "/ledger-website/index.html"
+  },
+  {
+    "revision": "0e1fc32127568340552d",
+    "url": "/ledger-website/js/app.78131774.js"
+  },
+  {
+    "revision": "1ee71a69568fa8e15711",
+    "url": "/ledger-website/js/chunk-vendors.11f8b486.js"
+  },
+  {
+    "revision": "576b0ed8d0e12a91eb18aef9d74a15c2",
+    "url": "/ledger-website/manifest.json"
+  },
+  {
+    "revision": "841283c3090fc9c4bcbecb3e095951fe",
+    "url": "/ledger-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ledger-website/robots.txt"
+  }
+]);
